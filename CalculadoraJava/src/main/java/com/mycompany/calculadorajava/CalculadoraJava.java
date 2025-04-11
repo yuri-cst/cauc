e7c05a0209ce8392c4efcbd975d80196c5c48a52
Yuri da Costa <yuri.santos@duque.g12.br>
2025-04-10 21:07:16 -0300

    projeto criado

---
+++ CalculadoraJava/src/main/java/com/mycompany/calculadorajava/CalculadoraJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorajava;
+
+/**
+ *
+ * @author 08240
+ */
+public class CalculadoraJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
